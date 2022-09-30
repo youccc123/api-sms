8304848e5b31ecde14a6700ac3a640cf3cd10415
Carlos Rodriguez <youccc123carlos@gmail.com>
2022-09-30 19:18:12 -0400

    actualizacion del Scrip

---
+++ db/Script.sql
@@ -0,0 +1,15 @@
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+
+SELECT * FROM roles;
+
+SELECT * FROM users;
+
+INSERT INTO users (email, password, username) VALUES('youccc123carlos@gmail.com', '123', 'carlos');
+
+SELECT * FROM personas ;
+INSERT INTO personas (idpersonas, apellido, documentonro, nombre) VALUES(3,'rodriguez', '3500591', 'carlos');
+
+
+eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjYXJsb3MiLCJpYXQiOjE2NjM5MDQzMDUsImV4cCI6MTY2Mzk5MDcwNX0.aG7bgsJ40RbP-LSR6AoNh_hAALn077BSGszCkUW7LdhbOGB168QkxjNfM4X64yQiHKlPVYruG_hbhW4PTN_dEQ
